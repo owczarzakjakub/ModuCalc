575438377fd8f6754173359d169e45ab1d7fc64b
Spring3r <krisomoriso@gmail.com>
2025-12-20 16:58:22 +0100

    Signed-off-by: Spring3r <krisomoriso@gmail.com>

--- build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug/ModuCalc_autogen/EWIEGA46WW/moc_mainwindow.cpp.d
+++ build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug/ModuCalc_autogen/EWIEGA46WW/moc_mainwindow.cpp.d
@@ -1,5 +1,5 @@
-C:/Users/kubao/Documents/MyProjectRepos/ModuCalc/build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug/ModuCalc_autogen/EWIEGA46WW/moc_mainwindow.cpp: C:/Users/kubao/Documents/MyProjectRepos/ModuCalc/mainwindow.h \
-  C:/Users/kubao/Documents/MyProjectRepos/ModuCalc/build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug/ModuCalc_autogen/moc_predefs.h \
+C:/Users/admin/Desktop/ModuCalc/build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug/ModuCalc_autogen/EWIEGA46WW/moc_mainwindow.cpp: C:/Users/admin/Desktop/ModuCalc/mainwindow.h \
+  C:/Users/admin/Desktop/ModuCalc/build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug/ModuCalc_autogen/moc_predefs.h \
   C:/Qt/6.10.0/mingw_64/include/QtCore/q17memory.h \
   C:/Qt/6.10.0/mingw_64/include/QtCore/q20functional.h \
   C:/Qt/6.10.0/mingw_64/include/QtCore/q20iterator.h \
