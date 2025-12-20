8804a2c73f6b656461d45526c54b9ce637bcd6da
Spring3r <krisomoriso@gmail.com>
2025-12-20 12:28:04 +0100

    dodawanie

--- src/types/vector.cpp
+++ src/types/vector.cpp
@@ -1,3 +1,18 @@
 #include "vector.h"
+#include <cmath>
+#include <stdexcept>
 
-Vector::Vector() {}
+Vector::Vector(const std::vector<double>& values):data(values){
+
+}
+Vector Vector::add(const Vector& v) const {
+    if (data.size() != v.data.size())
+        throw std::logic_error("Zły rozmiar wektorów");
+
+    std::vector<double> result;
+    for (int i = 0; i < data.size(); i++) {
+        result.push_back(data[i] + v.data[i]);
+    }
+
+    return Vector(result);
+}
