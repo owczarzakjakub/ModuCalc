bd108b63286cfaee80fb6c4c326ec647da0ffb9b
Spring3r <krisomoriso@gmail.com>
2026-02-22 19:19:33 +0100

    liczby zespolone i vektory dzialaja testy tez

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include "src/core/evaluator.h"
 #include "src/core/solver.h"
 #include "src/ocr/OCRReader.h"
-
+#include "src/types/complex.h"
 #include <QDebug>
 #include <vector>
 #include <iostream>
@@ -79,5 +79,41 @@ int main(int argc, char *argv[])
     double result4 = w.calculateExpression(eq4, hasVar);
     cout << "Test 4 - Result = " << result4 << endl;
 
+    cout<<"------ test vector---------"<<endl;
+    Vector v1({1, 2});
+    Vector v2({4, 5});
+    Vector suma = v1.add(v2);
+    cout<<"-- "<<v1.get(0);
+    cout << "Suma: ";
+    for (int i = 0; i < suma.size(); i++)
+        cout << suma.get(i) << " ";
+    cout << endl;
+    Vector roznica = v1.sub(v2);
+    cout << "Roznica: ";
+    for (int i = 0; i < suma.size(); i++)
+        cout << roznica.get(i) << " ";
+    cout << endl;
+    double iloczyn = v1.dot(v2);
+    cout << "Iloczyn skalarny: " << iloczyn <<endl;
+    cout << "Dlugosc v1: " << v1.length() << endl;
+
+    cout<<"------- zespolone testy ----"<<endl;
+    Complex z1(3, 4);
+    Complex z2(1, -2);
+
+    Complex csuma = z1.add(z2);
+    Complex croznica = z1.sub(z2);
+    Complex ciloczyn = z1.multi(z2);
+    Complex ciloraz = z1.div(z2);
+    Complex csprzez = z1.conjugate();
+
+    cout << "z1 + z2 = " << csuma.getReal() << "+" << csuma.getImag() << "i\n";
+    cout << "z1 - z2 = " << croznica.getReal() << "+" << croznica.getImag() << "i\n";
+    cout << "z1 * z2 = " << ciloczyn.getReal() << "+" << ciloczyn.getImag() << "i\n";
+    cout << "z1 / z2 = " << ciloraz.getReal() << "+" << ciloraz.getImag() << "i\n";
+    cout << "conjugate(z1) = " << csprzez.getReal() << "+" << csprzez.getImag() << "i\n";
+    cout << "magnitude(z1) = " << z1.magnitude() << "\n";
+    cout << "angle(z1) = " << z1.angle() << " rad\n";
+    cout << std::flush;
     return a.exec();
 }
